a3229754a065f1a3abf678fe2ef33346726ebdb9
sablyu <sablyu@live.com>
2022-03-12 13:53:22 +0500

    async task vs async void

---
+++ WebApplication1/AsyncFoo.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1
+{
+    public class AsyncFoo
+    {
+        public async Task DoFoo()
+        {
+            await Foo1();
+            Foo2();
+        }
+
+        public async Task<object> Foo1()
+        {
+            return await Task.FromResult<object>(null);
+        }
+
+        public async void Foo2()
+        {
+            await Task.FromResult<object>(null);
+        }
+    }
+}
