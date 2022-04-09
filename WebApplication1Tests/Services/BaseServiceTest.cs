ba579ca3cfdf2d682028443025e470d507e20ee9
Brian Jones <sablyu@live.com>
2022-04-09 10:13:33 +0200

    tests: initial tests project config

---
+++ WebApplication1Tests/Services/BaseServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Contexts;
+
+namespace WebApplication1Tests.Services
+{
+    public abstract class BaseTestService
+    {
+        public BaseTestService()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json",
+                    optional: false,
+                    reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            Options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseSqlServer(Configuration.GetConnectionString("DevDbConnection"))
+                .Options;
+        }
+
+
+        public IConfiguration Configuration { get; protected set; }
+        public virtual DbContextOptions<DatabaseContext> Options { get; protected set; }
+        public IServiceProvider ServiceProvider { get; protected set; }
+
+
+        protected virtual void InitServices()
+        {
+            var services = new ServiceCollection();
+            services.AddMemoryCache();
+
+            RegisterServices(services);
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+
+        protected virtual void InitDbOptions(DbContextOptionsBuilder builder) =>
+            builder.UseSqlServer(Configuration.GetConnectionString("DevDbConnection"));
+
+
+        protected virtual void RegisterServices(ServiceCollection services)
+        {
+            //DependencyInjector.RegisterServices(services);
+            services.AddSingleton<IConfiguration>(Configuration);
+            //services.AddDbContext<MainContext>(options => InitDbOptions(options));
+        }
+    }
+}
+
---
+++ WebApplication1Tests/Services/InMemoryServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Contexts;
+
+namespace WebApplication1Tests.Services
+{
+    public class InMemoryServiceTest : BaseTestService
+    {
+        public InMemoryServiceTest()
+        {
+            var dbBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+            InitDbOptions(dbBuilder);
+            Options = dbBuilder.Options;
+        }
+
+
+        protected virtual string DbName => this.GetType().Name;
+
+
+        protected override void InitDbOptions(DbContextOptionsBuilder builder) =>
+            builder.EnableSensitiveDataLogging().UseInMemoryDatabase(databaseName: DbName);
+    }
+}
+
